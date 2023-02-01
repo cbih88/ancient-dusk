552e5828b25969296cf3ebf397e176861ec7f4db
yoni <yoni.hasler@motorolasolutions.com>
2023-02-01 09:15:09 +0200

    Changes to VS code new files

--- app.js
+++ app.js
@@ -140,7 +140,10 @@ app.get("/about", function(req, res){
 });
 
 let port = process.env.PORT;
-// if (port == null || port == "") {  port = 3000;}
- 
- app.listen(port, function() {  console.log("Server started succesfully");}); 
+// if port == null || port == "") {
+//   port = 3000;
+// }
+app.listen(port || 3000, function() {
+  console.log("Server has started Successfully on dynamic port ");
+});
 
