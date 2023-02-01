8d37ce9395be58a5e0114bd45cb8830a242db6ac
yoni <yoni.hasler@motorolasolutions.com>
2023-02-01 09:10:48 +0200

    Changes to VS code second files

--- app.js
+++ app.js
@@ -139,12 +139,7 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
+let port = process.env.PORT;if (port == null || port == "") {  port = 3000;}
  
-app.listen(port, function() {
-  console.log("Server started succesfully");
-});
+ app.listen(port, function() {  console.log("Server started succesfully");}); 
 
