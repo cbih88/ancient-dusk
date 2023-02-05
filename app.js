7287ed96518112c17b64cc23ba2f2f17a1328ed8
yoni <yoni.hasler@motorolasolutions.com>
2023-02-05 08:19:08 +0200

    Changes app.js file

--- app.js
+++ app.js
@@ -139,6 +139,11 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+ 
+app.listen(port, function() {
+  console.log("Server started succesfully");
+}); 
