1b5afe7106dc0b750813f631819479c0ee632386
yoni <yoni.hasler@motorolasolutions.com>
2023-02-06 07:52:58 +0200

    change app and procfile

--- app.js
+++ app.js
@@ -151,6 +151,6 @@ if (PORT == null || PORT == "") {
     PORT = 3000;
 } 
 app.listen(PORT, () => {
-    console.log("server started on port ${PORT}");
+    console.log(`server started on port ${PORT}`);
   });
 
